c279dff3c03e6268cad0c32c7cf4153956f8a71b
Eddie Hu <ehu23@users.noreply.github.com>
2019-03-05 15:21:28 -0800

    Commit w/ animating squares

--- top.v
+++ top.v
@@ -12,14 +12,15 @@ module top(
     wire rst = RST_BTN;  // reset is active high on Basys3 (BTNC)
 
     // generate a 25 MHz pixel strobe
-    reg [15:0] cnt;
-    reg pix_stb;
+    reg [15:0] cnt = 0;
+    reg pix_stb = 0;
     always @(posedge CLK)
         {pix_stb, cnt} <= cnt + 16'h4000;  // divide by 4: (2^16)/4 = 0x4000
 
     wire [9:0] x;  // current pixel x position: 10-bit value: 0-1023
     wire [8:0] y;  // current pixel y position:  9-bit value: 0-511
-
+    wire animate;  // high when we're ready to animate at end of drawing
+    
     vga640x480 display (
         .i_clk(CLK),
         .i_pix_stb(pix_stb),
@@ -27,20 +28,60 @@ module top(
         .o_hs(VGA_HS_O), 
         .o_vs(VGA_VS_O), 
         .o_x(x), 
-        .o_y(y)
+        .o_y(y),
+        .o_animate(animate)
     );
 
-    // Four overlapping squares
-    wire sq_a, sq_b, sq_c, sq_d;
-    assign sq_a = ((x > 120) & (y >  40) & (x < 280) & (y < 200)) ? 1 : 0;
-    assign sq_b = ((x > 200) & (y > 120) & (x < 360) & (y < 280)) ? 1 : 0;
-    assign sq_c = ((x > 280) & (y > 200) & (x < 440) & (y < 360)) ? 1 : 0;
-    assign sq_d = ((x > 360) & (y > 280) & (x < 520) & (y < 440)) ? 1 : 0;
+    // 3 animating squares
+    wire sq_a, sq_b, sq_c;
+    wire [11:0] sq_a_x1, sq_a_x2, sq_a_y1, sq_a_y2;  // 12-bit values: 0-4095
+    wire [11:0] sq_b_x1, sq_b_x2, sq_b_y1, sq_b_y2;
+    wire [11:0] sq_c_x1, sq_c_x2, sq_c_y1, sq_c_y2;
 
-    assign VGA_R[2] = sq_b;         // square b is red
-    assign VGA_G[2] = sq_a | sq_d;  // squares a and d are green
-    assign VGA_B[1] = sq_c;         // square c is blue
-    
+    square #(.IX(160), .IY(120), .H_SIZE(60)) sq_a_anim (
+        .i_clk(CLK),
+        .i_ani_stb(pix_stb),
+        .i_rst(rst),
+        .i_animate(animate),
+        .o_x1(sq_a_x1),
+        .o_x2(sq_a_x2),
+        .o_y1(sq_a_y1),
+        .o_y2(sq_a_y2)
+    );
+
+    square #(.IX(320), .IY(240), .IY_DIR(0)) sq_b_anim (
+        .i_clk(CLK),
+        .i_ani_stb(pix_stb),
+        .i_rst(rst),
+        .i_animate(animate),
+        .o_x1(sq_b_x1),
+        .o_x2(sq_b_x2),
+        .o_y1(sq_b_y1),
+        .o_y2(sq_b_y2)
+    );
+
+    square #(.IX(480), .IY(360), .H_SIZE(100)) sq_c_anim (
+        .i_clk(CLK),
+        .i_ani_stb(pix_stb),
+        .i_rst(rst),
+        .i_animate(animate),
+        .o_x1(sq_c_x1),
+        .o_x2(sq_c_x2),
+        .o_y1(sq_c_y1),
+        .o_y2(sq_c_y2)
+    );
+
+    assign sq_a = ((x > sq_a_x1) & (y > sq_a_y1) &
+        (x < sq_a_x2) & (y < sq_a_y2)) ? 1 : 0;
+    assign sq_b = ((x > sq_b_x1) & (y > sq_b_y1) &
+        (x < sq_b_x2) & (y < sq_b_y2)) ? 1 : 0;
+    assign sq_c = ((x > sq_c_x1) & (y > sq_c_y1) &
+        (x < sq_c_x2) & (y < sq_c_y2)) ? 1 : 0;
+
+    assign VGA_R[2] = sq_a;  // square a is red
+    assign VGA_G[2] = sq_b;  // square b is green
+    assign VGA_B[1] = sq_c;  // square c is blue
+
     // Must fill in rest of bits in array for VGA. Default set to zero.
     assign VGA_R[0] = 1'b0;
     assign VGA_R[1] = 1'b0;
