462e1f50c3fbf208a29b8e7de40dd1e4ddd6b71e
Eddie Hu <ehu23@users.noreply.github.com>
2019-03-13 01:36:11 -0700

    Fix ship boundary bug and add ship shooting

--- ship.v
+++ ship.v
@@ -11,20 +11,38 @@ module ship #(
     input wire i_ani_stb,     // animation clock: pixel clock is 1 pix/frame
     input wire i_rst,         // reset: returns animation to starting position
     input wire i_animate,     // animate when input is high
-    input wire [3:0] sw,      // movement switches
-    output wire [11:0] o_x1,  // square left edge: 12-bit value: 0-4095. We use 12 bits so this module can be used on 4k as well.
-    output wire [11:0] o_x2,  // square right edge
-    output wire [11:0] o_y1,  // square top edge
-    output wire [11:0] o_y2   // square bottom edge
+    input wire [7:0] i_sw,      // movement switches
+    output wire [11:0] o_x1,  // player left edge: 12-bit value: 0-4095. We use 12 bits so this module can be used on 4k as well.
+    output wire [11:0] o_x2,  // player right edge
+    output wire [11:0] o_y1,  // player top edge
+    output wire [11:0] o_y2   // player bottom edge
+    output wire [11:0] o_bx1, // bullet left edge 
+    output wire [11:0] o_bx2, // bullet right edge
+    output wire [11:0] o_by1, // bullet top edge
+    output wire [11:0] o_by2, // bullet bottom edge
+    output wire o_firing      // high when bullet is fired
     );
 
-    reg [11:0] x = IX;   // horizontal position of square centre
-    reg [11:0] y = IY;   // vertical position of square centre
+    reg [11:0] x = IX;   // horizontal position of player centre
+    reg [11:0] y = IY;   // vertical position of player centre
+
+    // For now, assigning bullet position to player initially
+    reg [11:0] bx = IX;
+    reg [11:0] by = IY;
+    reg in_air = 0;
 
     assign o_x1 = x - H_SIZE;  // left: centre minus half horizontal size
     assign o_x2 = x + H_SIZE;  // right
     assign o_y1 = y - H_SIZE;  // top
     assign o_y2 = y + H_SIZE;  // bottom
+
+    // For now, assign the bullet to be the same as the player size
+    assign o_bx1 = bx - H_SIZE;
+    assign o_bx2 = bx + H_SIZE;
+    assign o_by1 = by - H_SIZE;
+    assign o_by2 = by + H_SIZE;
+
+    assign o_firing = in_air;
 
     always @ (posedge i_clk)
     begin
@@ -32,26 +50,48 @@ module ship #(
         begin
             x <= IX;
             y <= IY;
+            bx <= IX;
+            by <= IY;
+            in_air = 0;
         end
         if (i_animate && i_ani_stb)
         begin
-            if (sw[0])
-                x <= x - 1'b1;
-            if (sw[3])
+            // Player Movement
+            if (i_sw[0]) // Right
                 x <= x + 1'b1;
-            if (sw[1])
+            if (i_sw[7]) // Left
+                x <= x - 1'b1;
+            if (i_sw[6]) // Up
                 y <= y - 1'b1;
-            if (sw[2])
+            if (i_sw[1]) // Down
                 y <= y + 1'b1;
 
+
+            // Bullet logic
+            if (i_sw[4] & ~in_air) // If we press shoot and the bullet is not in the air
+                in_air = 1;
+
+            if (in_air) // If bullet is in the air
+                by <= by - 1'b1; // Move bullet up
+
+            // Player Boundary control:
             if (x <= H_SIZE + 1'b1)  // edge of square is at left of screen
-                x <= H_SIZE + 1'b1;  // stay put
+                x <= H_SIZE + 2'b10);
             if (x >= (D_WIDTH - H_SIZE - 1'b1))  // edge of square at right
-                x <= D_WIDTH - H_SIZE - 1'b1;  // stay put          
+                x <= D_WIDTH - H_SIZE - 2'b10;            
             if (y <= H_SIZE + 1'b1)  // edge of square at top of screen
-                y <= H_SIZE + 1'b1;  // stay put
+                y <= H_SIZE + 2'b10;  
             if (y >= (D_HEIGHT - H_SIZE - 1'b1))  // edge of square at bottom
-                y <= D_HEIGHT - H_SIZE - 1'b1;  // stay put              
+                y <= D_HEIGHT - H_SIZE - 2'b10;                
+
+            // Bullet Boundary Detection:
+            if ((bx <= H_SIZE + 1'b1) || (bx >= (D_WIDTH - H_SIZE - 1'b1)) || (by <= H_SIZE + 1'b1) || (by >= (D_HEIGHT - H_SIZE - 1'b1)))
+            begin
+                in_air = 0;
+                by <= y;
+                bx <= x;
+            end
+
         end
     end
 endmodule
--- top.v
+++ top.v
@@ -1,7 +1,7 @@
 module top(
     input wire CLK,             // board clock: 100 MHz on Arty/Basys3/Nexys
     input wire RST_BTN,         // reset button
-    input wire [3:0] sw,        // 4 movement controls
+    input wire [7:0] sw,        // 4 movement controls
     output wire VGA_HS_O,       // horizontal sync output
     output wire VGA_VS_O,       // vertical sync output
     output wire [2:0] VGA_R,    // 3-bit VGA red output
@@ -9,7 +9,6 @@ module top(
     output wire [1:0] VGA_B     // 2-bit VGA blue output
     );
 
-    // wire rst = ~RST_BTN;    // reset is active low on Arty & Nexys Video
     wire rst = RST_BTN;  // reset is active high on Basys3 (BTNC) and Nexys 3
 
     // generate a 25 MHz pixel strobe. So a clock that is four times slower.
@@ -37,23 +36,38 @@ module top(
     wire player_ship;
     wire [11:0] player_x1, player_x2, player_y1, player_y2;  // 12-bit values: 0-4095
 
-    ship #(.H_SIZE(40)) player (
+    // Player's bullet 
+    wire player_bullet; 
+    wire firing;
+    wire [11:0] bullet_x1, bullet_x2, bullet_y1, bullet_y2;  // 12-bit values: 0-4095
+
+    ship #(.H_SIZE(20)) player (
         .i_clk(CLK),
         .i_ani_stb(pix_stb),
         .i_rst(rst),
         .i_animate(animate),
-        .sw(sw),
+        .i_sw(sw),
         .o_x1(player_x1),
         .o_x2(player_x2),
         .o_y1(player_y1),
-        .o_y2(player_y2)
+        .o_y2(player_y2),
+        .o_bx1(bullet_x1),
+        .o_bx2(bullet_x2),
+        .o_by1(bullet_y1),
+        .o_by2(bullet_y2),
+        .o_firing(firing)
     );
 
     assign player_ship = ((x > player_x1) & (y > player_y1) &
         (x < player_x2) & (y < player_y2)) ? 1'b1 : 1'b0;
 
+    // Color in the bullet as long as its firing/intheair
+    assign player_bullet = ((x > bullet_x1) & (y > bullet_y1) &
+        (x < bullet_x2) & (y < bullet_y2) & (firing)) ? 1'b1 : 1'b0;
+ 
+    // Designate colors:
     assign VGA_R[2] = player_ship;  // player_ship is red
-    assign VGA_G[2] = 1'b0;  
+    assign VGA_G[2] = player_bullet;// player_bullet is green  
     assign VGA_B[1] = 1'b0; 
 
     // Must fill in rest of bits in array for VGA. Default set to zero.
